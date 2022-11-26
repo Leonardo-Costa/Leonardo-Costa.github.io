d98d854b8a0490d231d5473c0c2ac1a7b0a9e6a0
Leonardo Costa <leonardo.costa@unifesp.br>
2022-11-25 21:02:58 -0300

    adicionando curriculo

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+  "https://raw.githubusercontent.com/Leonardo-Costa/portfolio/blob/main/src/Assets/Leonardo%20Loureiro%20Costa-CV.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
